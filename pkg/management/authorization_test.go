91a590b05c3fb8ca57994da3f597e694f4807ad6
Francis PEROT <francis.perot@elca.ch>
2020-05-20 15:16:38 +0200

    [CLOUDTRUST-2674] Add method to export users

--- pkg/management/authorization_test.go
+++ pkg/management/authorization_test.go
@@ -131,6 +131,9 @@ func TestDeny(t *testing.T) {
 		_, err = authorizationMW.GetRequiredActions(ctx, realmName)
 		assert.Equal(t, security.ForbiddenError{}, err)
 
+		_, err = authorizationMW.ExportUsers(ctx, realmName)
+		assert.Equal(t, security.ForbiddenError{}, err)
+
 		err = authorizationMW.DeleteUser(ctx, realmName, userID)
 		assert.Equal(t, security.ForbiddenError{}, err)
 
@@ -411,6 +414,10 @@ func TestAllowed(t *testing.T) {
 
 		mockManagementComponent.EXPECT().GetRequiredActions(ctx, realmName).Return([]api.RequiredActionRepresentation{}, nil)
 		_, err = authorizationMW.GetRequiredActions(ctx, realmName)
+		assert.Nil(t, err)
+
+		mockManagementComponent.EXPECT().ExportUsers(ctx, realmName).Return([]api.UserExportRepresentation{}, nil)
+		_, err = authorizationMW.ExportUsers(ctx, realmName)
 		assert.Nil(t, err)
 
 		mockManagementComponent.EXPECT().DeleteUser(ctx, realmName, userID).Return(nil)
